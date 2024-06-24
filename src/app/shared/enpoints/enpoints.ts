805b264d158e536be6c1574d205d1788285a7f64
Franco Herrera <franncoherrera2011@gmail.com>
2024-06-24 16:12:32 -0300

    feature/REGISTER: text-area component added

--- src/app/shared/enpoints/enpoints.ts
+++ src/app/shared/enpoints/enpoints.ts
@@ -1,3 +1,8 @@
 export const ENDPOINTS = {
   LOGIN_SESSION: '/iniciarsesion',
+
+  COUNTRY_ACTIVE: '/paises/listarpaisesactivos',
+  LANGUAGE_ACTIVE: '/idiomas/listaridiomasactivos',
+  LANGUAGE_LEVEL_ACTIVE: '/nivelesidioma/listarnivelesidiomaactivos',
+  INTERESTS_ACTIVE: '/intereses/listarinteresesactivos'
 };
