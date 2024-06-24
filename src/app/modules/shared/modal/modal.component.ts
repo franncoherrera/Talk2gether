f82309ca793b279a908e7bf4984097c249479fb6
Franco Herrera <franncoherrera2011@gmail.com>
2024-06-24 19:44:27 -0300

    feature/REGISTER: modal added

---
+++ src/app/modules/shared/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'fhv-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss',
+})
+export class ModalComponent {
+  @Input() loadModal: boolean;
+  @Input() modalTitle: string;
+  @Output() loadComponent: EventEmitter<boolean> = new EventEmitter();
+
+  closeModal() {
+    this.loadComponent.emit(false);
+  }
+}
