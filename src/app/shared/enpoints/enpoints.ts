259db8740c1c3e4265ef5a4dd4ff08f4f82709cc
Franco Herrera <franncoherrera2011@gmail.com>
2024-07-06 11:19:47 -0300

    deploy: files parameters .json created

--- src/app/shared/enpoints/enpoints.ts
+++ src/app/shared/enpoints/enpoints.ts
@@ -4,8 +4,20 @@ export const ENDPOINTS = {
   /* Register endpoint */
   REGISTER_USER: '/usuario/registrarse',
   /* Params endpoints */
-  COUNTRY_ACTIVE: '/paises/listarpaisesactivos',
-  LANGUAGE_ACTIVE: '/idiomas/listaridiomasactivos',
-  LANGUAGE_LEVEL_ACTIVE: '/nivelesidioma/listarnivelesidiomaactivos',
-  INTERESTS_ACTIVE: '/intereses/listarinteresesactivos'
+  COUNTRY_ACTIVE: '/paises/listarpaisesactivos.json',
+  LANGUAGE_ACTIVE: '/idiomas/listaridiomasactivos.json',
+  LANGUAGE_LEVEL_ACTIVE: '/nivelesidioma/listarnivelesidiomaactivos.json',
+  INTERESTS_ACTIVE: '/intereses/listarinteresesactivos.json'
 };
+// export const ENDPOINTS = {
+//   /*Login endpoint*/
+//   LOGIN_SESSION: '/iniciarsesion',
+//   /* Register endpoint */
+//   REGISTER_USER: '/usuario/registrarse',
+//   /* Params endpoints */
+//   COUNTRY_ACTIVE: '/paises/listarpaisesactivos',
+//   LANGUAGE_ACTIVE: '/idiomas/listaridiomasactivos',
+//   LANGUAGE_LEVEL_ACTIVE: '/nivelesidioma/listarnivelesidiomaactivos',
+//   INTERESTS_ACTIVE: '/intereses/listarinteresesactivos'
+// };
+
