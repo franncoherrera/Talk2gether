13e007571a8de665c86d847bf327569a69cc0ba5
Franco Herrera <franncoherrera2011@gmail.com>
2024-07-12 15:06:18 -0300

    feature/PAGINATE: main page paginated

--- src/app/modules/common/common-main-page/components/classic-card/classic-card.module.ts
+++ src/app/modules/common/common-main-page/components/classic-card/classic-card.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClassicCardComponent } from './classic-card.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [ClassicCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NgxPaginationModule],
   exports: [ClassicCardComponent],
 })
 export class ClassicCardModule {}
