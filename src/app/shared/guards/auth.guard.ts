e68d7c2ffcd70524ba7520d5283e597288f3daf2
Franco Herrera <franncoherrera2011@gmail.com>
2024-07-16 19:48:23 -0300

    deploy: authGuard fix

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -11,12 +11,13 @@ export const authGuard: CanActivateFn = () => {
   const router = inject(Router);
   const sweetAlertService = inject(SweetAlertService);
   const translateService = inject(TranslateService);
-  if (sesionService.isLoggedIn()) {
-    router.navigate([GENERAL_PATH.MAIN_PATH]);
+  const currentSession :string = sesionService.getCurrentSesion(); 
+  if (currentSession !== null) {
     sweetAlertService.alertMessageConfirm(
       translateService.instant('common.error.general_error_logged'),
       SWEET_ALERT_ICON.ERROR
     );
+    router.navigate([GENERAL_PATH.MAIN_PATH]);
     return false;
   } else {
     return true;
