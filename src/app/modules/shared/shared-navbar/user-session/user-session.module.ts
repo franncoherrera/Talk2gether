21b9895fb81dea2f03fa6f6b44bc818c1e90e084
Franco Herrera <franncoherrera2011@gmail.com>
2024-07-16 20:08:23 -0300

    deploy: authGuard fix

--- src/app/modules/shared/shared-navbar/user-session/user-session.module.ts
+++ src/app/modules/shared/shared-navbar/user-session/user-session.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserSessionComponent } from './user-session.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [UserSessionComponent],
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, RouterModule],
   exports: [UserSessionComponent],
 })
 export class UserSessionModule {}
