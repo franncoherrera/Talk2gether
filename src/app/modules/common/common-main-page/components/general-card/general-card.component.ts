a905171ff07b4e7ce2e718e0d8fdee2a93bfc463
Franco Herrera <franncoherrera2011@gmail.com>
2024-07-27 15:57:55 -0300

    feature/VIDEOCALL: translations added and initial pop-up done

--- src/app/modules/common/common-main-page/components/general-card/general-card.component.ts
+++ src/app/modules/common/common-main-page/components/general-card/general-card.component.ts
@@ -4,6 +4,9 @@ import { PAGINATION } from '../../../../../shared/constants/paginationConstants'
 import { MainPageService } from '../../services/main-page.service';
 import { Router } from '@angular/router';
 import { ROUTES_PATH } from '../../../../../shared/constants/routes';
+import Swal from 'sweetalert2';
+import { SWEET_ALERT_ICON } from '../../../../../shared/enums/sweeAlert.enum';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'fhv-general-card',
@@ -19,10 +22,39 @@ export class GeneralCardComponent {
 
   protected mainPageService: MainPageService = inject(MainPageService);
   protected router: Router = inject(Router);
+  protected translateService: TranslateService = inject(TranslateService);
 
   protected goVideoCall(urlPhoto: string): void {
-    
-    this.mainPageService.saveUrlPhoto(urlPhoto);
-    this.router.navigate([ROUTES_PATH.VIDEO_CALL_PAGE]);
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success ms-5',
+        cancelButton: 'btn btn-danger',
+      },
+      buttonsStyling: false,
+    });
+    swalWithBootstrapButtons
+      .fire({
+        title: this.translateService.instant(
+          'common.video_call_page.video_call_modal_title'
+        ),
+        text: this.translateService.instant(
+          'common.video_call_page.video_call_modal_subtitle'
+        ),
+        icon: SWEET_ALERT_ICON.WARNING,
+        showCancelButton: true,
+        confirmButtonText: this.translateService.instant(
+          'common.video_call_page.video_call_modal_button_continue'
+        ),
+        cancelButtonText: this.translateService.instant(
+          'common.video_call_page.video_call_modal_button_back'
+        ),
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.mainPageService.saveUrlPhoto(urlPhoto);
+          this.router.navigate([ROUTES_PATH.VIDEO_CALL_PAGE]);
+        }
+      });
   }
 }
