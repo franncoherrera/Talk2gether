028aa81d9fe1b16b3cdf988bb8c473f8c11d69f6
Franco Herrera <franncoherrera2011@gmail.com>
2024-07-26 23:40:55 -0300

    feature/VIDEOCALL: redirect call done

--- src/app/modules/common/common-main-page/components/general-card/general-card.component.ts
+++ src/app/modules/common/common-main-page/components/general-card/general-card.component.ts
@@ -1,16 +1,27 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, inject, input, ViewEncapsulation } from '@angular/core';
 import { ROOM_USER } from '../../../../../shared/models/roomUser.model';
 import { PAGINATION } from '../../../../../shared/constants/paginationConstants';
+import { MainPageService } from '../../services/main-page.service';
+import { Router } from '@angular/router';
+import { ROUTES_PATH } from '../../../../../shared/constants/routes';
 
 @Component({
   selector: 'fhv-general-card',
   templateUrl: './general-card.component.html',
   styleUrl: './general-card.component.scss',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class GeneralCardComponent {
   readonly PAGINATION = PAGINATION;
   page: number;
-  userRoom = input.required<ROOM_USER[]>();
-  isClassicVersion = input.required<boolean>();
+  userRoom = input<ROOM_USER[]>();
+  isClassicVersion = input<boolean>();
+
+  protected mainPageService: MainPageService = inject(MainPageService);
+  protected router: Router = inject(Router);
+
+  protected goVideoCall(urlPhoto: string): void {
+    this.mainPageService.saveUrlPhoto(urlPhoto);
+    this.router.navigate([ROUTES_PATH.VIDEO_CALL_PAGE]);
+  }
 }
